ad6569c072e009767e5d5029d9166ffae95c9ed5
Ibtid <ibtid011@example.com>
2022-03-04 21:59:51 +0600

    gallery

--- client/pages/gallery.js
+++ client/pages/gallery.js
@@ -12,7 +12,7 @@ const gallery = () => {
   return (
     <div className='min-w-screen min-h-screen bg-white pt-[6rem] md:pt-[7rem]'>
       {/*====================================================== Gallery and buttons ========================================================*/}
-      <div className='ml-[3vw] flex mr-[3vw] items-center justify-between pb-2 md:pb-5 border-b-[1px] border-gray'>
+      <div className='bg-white ml-[3vw] flex mr-[3vw] items-center justify-between pb-2 md:pb-5 border-b-[1px] border-gray sticky top-20 z-10'>
         <div className='font-Orbitron md:text-[1.75rem] 2xl:text-[2rem]'>
           GALLERY
         </div>
