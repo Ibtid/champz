53b28fc070bea5d9d278a9aca7e4b89f1a622b4c
Ibtid <ibtid011@example.com>
2022-03-05 18:34:57 +0600

    fixing nav

--- client/components/FilterMenu/FilterItems.js
+++ client/components/FilterMenu/FilterItems.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import Image from 'next/image';
-import srcpic from '../../public/assets/icons/infographic-elements 1.svg';
+import add from '../../public/assets/icons/add.svg';
 
-const FilterItems = () => {
+const FilterItems = (props) => {
   return (
-    <div className='flex items-center justify-center'>
+    <div className='flex items-center justify-between p-2  border-b-[1px] border-gray'>
       <div className='flex items-center justify-center mr-20'>
         <div className='h-[1rem] w-[1rem] mr-2'>
-          <Image src={srcpic} layout='responsive' />
+          <Image src={props.srcpic} layout='responsive' />
         </div>
-        <div>ELEMENT</div>
+        <div>{props.content}</div>
       </div>
-      <div className='h-[1rem] w-[1rem]'>
-        <Image src={srcpic} layout='responsive' />
+      <div className='h-[0.8rem] w-[0.8rem]'>
+        <Image src={add} layout='responsive' />
       </div>
     </div>
   );
--- client/components/Navbar/Navbar.js
+++ client/components/Navbar/Navbar.js
@@ -1,10 +1,20 @@
 import Image from 'next/image';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from '../../public/assets/Logo/Steve_kim_champz_logo_with_black_bg (2) 1.svg';
+import { useRouter } from 'next/router';
 
 const Navbar = () => {
+  const [background, setBackground] = useState('bg-black text-white');
+
+  const router = useRouter();
+
+  useEffect(() => {
+    console.log(window.location.pathname);
+  }, [router.pathname]);
+
   return (
-    <div className='bg-white w-screen h-[5rem] z-20 flex items-center justify-between fixed top-0 left-0'>
+    <div
+      className={`${background} w-screen h-[5rem] z-20 flex items-center justify-between fixed top-0 left-0`}>
       <div className='ml-[3vw] w-[6.5rem] mt-6 2xl:w-[8rem] md:ml-[3vw]'>
         {/*<img
           className='h-[4rem] w-[5rem] md:h-[5rem] md:w-[6.5rem]'
@@ -18,12 +28,52 @@ const Navbar = () => {
         <div className='h-[0.25rem] w-[2.25rem] bg-red mt-[0.25rem] mb-[0.25rem] rounded-sm'></div>
         <div className='h-[0.25rem] w-[2.25rem] bg-red rounded-sm'></div>
       </div>
-      <div className='hidden md:flex text-black mr-[3vw] items-center font-Roboto xl:text-base 2xl:text-lg '>
-        <div className='ml-8 cursor-pointer'>HOME</div>
-        <div className='ml-8 cursor-pointer'>THE ARENA</div>
-        <div className='ml-8 cursor-pointer'>FIGHT CLUB</div>
-        <div className='ml-8 cursor-pointer'>ROADMAP</div>
-        <div className='ml-8 cursor-pointer'>GALLERY</div>
+      <div className='hidden md:flex mr-[3vw] items-center font-Roboto xl:text-base 2xl:text-lg '>
+        <div
+          className='ml-8 cursor-pointer'
+          onClick={() => {
+            setBackground('bg-black text-white');
+
+            router.push('/');
+          }}>
+          HOME
+        </div>
+        <div
+          className='ml-8 cursor-pointer'
+          onClick={() => {
+            setBackground('bg-blue text-white');
+
+            router.push('/arena');
+          }}>
+          THE ARENA
+        </div>
+        <div
+          className='ml-8 cursor-pointer'
+          onClick={() => {
+            setBackground('bg-white text-black');
+
+            router.push('/fightClub');
+          }}>
+          FIGHT CLUB
+        </div>
+        <div
+          className='ml-8 cursor-pointer'
+          onClick={() => {
+            setBackground('bg-white text-black');
+
+            router.push('/roadmap');
+          }}>
+          ROADMAP
+        </div>
+        <div
+          className='ml-8 cursor-pointer'
+          onClick={() => {
+            setBackground('bg-white text-black');
+
+            router.push('/gallery');
+          }}>
+          GALLERY
+        </div>
         <div className='ml-8 cursor-pointer'>SHOP</div>
       </div>
     </div>
