86e002f7329626f890933eb8414cdb7284361b5b
Ibtid <ibtid011@example.com>
2022-03-06 13:46:27 +0600

    fightclub done

--- client/pages/fightclub.js
+++ client/pages/fightclub.js
@@ -14,23 +14,29 @@ const fightclub = () => {
       <div className='font-Orbitron  text-red text-bg font-bold absolute top-[25vh] md:top-[22vh] right-[20vw] md:right-[26vw] text-2xl md:text-5xl 2xl:text-7xl'>
         CLUB
       </div>
-      <div className='absolute top-[45vh] flex flex-wrap items-center justify-center'>
+      <div className='absolute top-[45vh] md:top-[48vh] flex flex-wrap items-center justify-center'>
         <div className='flex items-center justify-center'>
-          <div className='font-Orbitron text-6xl text-gray mr-5'>1</div>
-          <div className='w-[10rem] text-xs font-Roboto text-black'>
+          <div className='font-Orbitron text-6xl md:text-9xl text-gray mr-5'>
+            1
+          </div>
+          <div className='w-[10rem] md:w-[13rem] text-xs md:text-base font-Roboto text-black'>
             We are all ChampionZ in life
           </div>
         </div>
         <div className='flex items-center justify-center mr-3 ml-3 mt-5 mb-5'>
-          <div className='font-Orbitron text-6xl text-gray mr-5'>2</div>
-          <div className='w-[10rem]  text-xs font-Roboto text-black'>
+          <div className='font-Orbitron text-6xl md:text-9xl text-gray mr-5'>
+            2
+          </div>
+          <div className='w-[10rem] md:w-[13rem] text-xs md:text-base font-Roboto text-black'>
             No ChampionZ can make the journey on their own. We surround
             ourselves with other ChampionZ
           </div>
         </div>
         <div className='flex items-center justify-center m-3'>
-          <div className='font-Orbitron text-6xl text-gray mr-5'>3</div>
-          <div className='w-[10rem]  text-xs font-Roboto text-black'>
+          <div className='font-Orbitron text-6xl md:text-9xl text-gray mr-5'>
+            3
+          </div>
+          <div className='w-[10rem] md:w-[13rem] text-xs md:text-base font-Roboto text-black'>
             We fight till the end. Don't quit and live the rest of your lives as
             ChampionZ. #WAGMI
           </div>
