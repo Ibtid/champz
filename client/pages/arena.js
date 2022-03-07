3d3dafd91dfaf74767be58630d560b3dfe600415
Ibtid <ibtid011@example.com>
2022-03-07 22:29:32 +0600

    fixes done

--- client/pages/arena.js
+++ client/pages/arena.js
@@ -1,31 +1,46 @@
-import React from "react";
+import React from 'react';
 
 const arena = () => {
   return (
-    <div className="arena_screen">
-      <div className="main_arena_container">
-        <div className="left_arena_container">
-          <div className="arena_heading_white">We step into the</div>
-          <div className="arena_heading_red">Arena.</div>
-          <div className="arena_body_text">
-            We fight till the end. <br /> <br />
-            We rise as ChampZ. <br /> <br />
-            10,000 ChampionZ come together to conquer the world.
-            <br /> <br />
-            We bond with one another and we build the community together.
-            <br /> <br />
-            The Arena gives all the ChampionZ access to special events, <br />
-            secret channels, dope merchandise, and much more.
+    <div className='arena_screen'>
+      <div className='main_arena_container'>
+        <div className='left_arena_container'>
+          <div>
+            <div className='arena_heading_white'>We step into the</div>
+            <div className='arena_heading_red'>Arena.</div>
           </div>
-          
-          <div className="arena_end_text_white">
-            Do you have what it takes to
-            be a <p className="arena_end_text_red">Champion?</p> 
-          </div>
-        </div>
-        <div className="arena_right_container">
+          <div className='arena_body_text'>
+            <div className=' md:text-lg 2xl:text-2xl mb-1 md:mb-3 2xl:mb-5'>
+              We fight till the end.{' '}
+            </div>
+            <div className='md:text-lg 2xl:text-2xl mb-1 md:mb-3 2xl:mb-5'>
+              We rise as ChampZ.{' '}
+            </div>
+            <div className='md:text-lg 2xl:text-2xl mb-1 md:mb-3 2xl:mb-5'>
+              10,000 ChampionZ come together to conquer the world.
+            </div>
+            <div className='md:text-lg  2xl:text-2xl mb-1 md:mb-3 2xl:mb-5 '>
+              {' '}
+              We bond with one another and we build the community together.{' '}
+            </div>
 
+            <div className='md:text-lg 2xl:text-2xl'>
+              The Arena gives all the ChampionZ access to special events,{' '}
+            </div>
+            <div className='md:text-lg 2xl:text-2xl mb-1 md:mb-3 2xl:mb-5 '>
+              secret channels, dope merchandise, and much more.
+            </div>
+          </div>
+
+          <div className=' arena_end_text_white'>
+            <div> Do you have what it takes to</div>
+            <div>
+              {' '}
+              be a <span className=' arena_end_text_red'>Champion? </span>
+            </div>
+          </div>
         </div>
+        <div className='arena_right_container'></div>
       </div>
     </div>
   );
