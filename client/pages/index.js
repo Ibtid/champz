d03f48e871214495445c32a251cd1032a950ee52
Ibtid <ibtid011@example.com>
2022-03-07 21:45:38 +0600

    index fixes

--- client/pages/index.js
+++ client/pages/index.js
@@ -3,15 +3,19 @@ import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
-
   return (
     <div className='index_container'>
-      <img className='index_background_cover' src="https://i.ibb.co/JtJ1GpY/Homepage.png" alt='cover-pic'/>
-      <div className='index_heading_line1'>Do you have what it takes to be &nbsp;<div style={{color:'#C62f41'}}>Champion?</div> </div>
-      {/* <div className='index_heading_line2'>
-      <div className='index_heading_white'>to be Champion?</div>
-      <div className='index_heading_red'>Champion?</div>
-      </div> */}
+      <img
+        className='index_background_cover'
+        src='https://i.ibb.co/JtJ1GpY/Homepage.png'
+        alt='cover-pic'
+      />
+      <div className='index_heading_line1'>
+        <div>Do you have what it takes to </div>
+        <div>
+          be a <span style={{ color: '#C62f41' }}>Champion?</span>
+        </div>
+      </div>
     </div>
   );
 }
